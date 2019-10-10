595be2dd5bf9da791ca1729fd654879dd11ea4c7
寻找海蓝 <18868832272@163.com>
2019-10-11 01:10:07 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,30 @@
+const merge = require("webpack-merge");
+const tsImportPluginFactory = require("ts-import-plugin");
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: true,
+  chainWebpack: config => {
+    config.resolve.alias.set("@", resolve("src")),
+      config.module
+        .rule("ts")
+        .use("ts-loader")
+        .tap(options => {
+          options = merge(options, {
+            transpileOnly: true,
+            getCustomTransformers: () => ({
+              before: [
+                tsImportPluginFactory({
+                  libraryName: "vant",
+                  libraryDirectory: "es",
+                  style: true
+                })
+              ]
+            }),
+            compilerOptions: {
+              module: "es2015"
+            }
+          });
+          return options;
+        });
+  }
 };
